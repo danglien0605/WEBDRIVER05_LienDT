5d020a38971b01f9644c3b2eb3b762ef526d7e83
Đặng Thị Liên <danglien0605@gmail.com>
2020-12-28 23:08:11 +0700

    init

---
+++ src/java_API/topic_01_checkenv.java
@@ -0,0 +1,39 @@
+package java_API;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+
+public class topic_01_checkenv {
+	WebDriver webdriver;
+  @Test
+  public void checkURL_and_title() {
+	 String title = webdriver.getTitle();
+	 AssertJUnit.assertEquals(title, "Guru99 Bank Homennnnnnn Page");
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  webdriver  = new ChromeDriver();
+	  webdriver.get("http://demo.guru99.com/V4/");
+	  webdriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  webdriver.manage().window().maximize();
+	  
+	  
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  webdriver.quit();
+  }
+
+}
